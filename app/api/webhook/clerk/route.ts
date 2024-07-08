0ffa14f947cdf0e5f61f4366f2b520b47d450e59
Shreyash Wagh <shreyashwagh22@gmail.com>
2024-07-08 21:32:30 +0530

    second

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,38 +1,39 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
+import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
-
+ 
 export async function POST(req: Request) {
-
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+ 
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-
+ 
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
-
+ 
   // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
-
+ 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
   }
-
+ 
   // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
-
+ 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
-
+ 
   let evt: WebhookEvent
  
   // Verify the payload with the headers
@@ -60,8 +61,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name ?? "",
+      lastName: last_name ?? "",
       photo: image_url,
     }
 
@@ -82,8 +83,8 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name ?? '',
+      lastName: last_name ?? '',
       username: username!,
       photo: image_url,
     }
@@ -103,3 +104,4 @@ export async function POST(req: Request) {
  
   return new Response('', { status: 200 })
 }
+ 
